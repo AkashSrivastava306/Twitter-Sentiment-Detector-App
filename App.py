0ba23b300bdbdde7454eadcee23e1628d83a17c3
Akash Srivastava <60046522+AkashSrivastava306@users.noreply.github.com>
2021-08-02 02:21:31 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,30 @@
+# Importing Flask lib
+
+from flask import Flask, render_template, url_for, request
+import pickle
+import pandas as pd
+import joblib
+
+filename = open('pickle.pkl', 'rb')
+clf = joblib.load(filename)
+test = open('transform.pkl.', 'rb')
+cv = joblib.load(test)
+app = Flask(__name__, template_folder='template')
+@app.route('/')
+
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        test_vector = cv.transform(data)
+        my_prediction = clf.predict(test_vector)
+    return render_template('result.html', prediction=my_prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
